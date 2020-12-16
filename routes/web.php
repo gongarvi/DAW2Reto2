410e9a0f276e2cc914d27bf750a940f426fa9b2d
ik_2dw3 <ik_2dw3@H015110.NET>
2020-12-16 13:13:51 +0100

    estructura menu completada, falta unir con demás elementos

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('', function () {
     return view('welcome');
-});
+})->name("inicio");
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 
 //Abrir nuevo enlace a menu juegos y juegos
 Route::get("/juegos/matching", function(){
@@ -29,9 +30,18 @@ Route::get("/juegos/matching", function(){
 })->name("matching");
 
 Route::get("/juegos/buscaminas", function(){
-    return view("matching");
+    return view("home");
 })->name("buscaminas");
 
-Route::get("/juegos",[GameController::class,"show"]);
+Route::get("/juegos",[GameController::class,"show"])->name("juegos");
+
+Route::get("/mujeres",[GameController::class,"show"])->name("mujeres");
+
+Route::get("/perfil",[GameController::class,"show"])->name("perfil");
+
+Route::get("/logout",[GameController::class,"show"])->name("logout");
+
+
+
 
 
