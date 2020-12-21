ea7292eb0c9fff5fb07fba893d1e1d25c804f752
Sayeeda12614 <sayeeda.txurdi@gmail.com>
2020-12-21 11:25:15 +0100

    21/12/2020

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id('id_usuario'); // id del usuario
+            $table->string('nombre'); 
+            $table->string('apellidos');
+            $table->string('email')->unique();  
+            $table->timestamp('email_verified_at')->nullable(); // para verificar el correo
             $table->string('password');
+            $table->string('foto_perfil');
+            $table->integer('progreso');
+            $table->boolean('administrador')->default(0); 
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2020_12_14_074541_create_questions.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Reference\Reference;
+
+class CreateQuestions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id('id_preguntas');
+            $table->string('pregunta');
+            $table->string('respuesta'); 
+            $table->foreign('mujer')->references('id_mujer')->on('womens');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
