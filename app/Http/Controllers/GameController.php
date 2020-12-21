38bd9e1d14137331a3e251f53c1097bfaa8abd8b
Chac092 <45790760+Chac092@users.noreply.github.com>
2020-12-21 11:23:44 +0100

    Completar el array de los jeugos con las descripciones

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -13,13 +13,28 @@ class GameController extends Controller
         $this->juegos = array(
             array(
                 "nombre"=>"matching",
-                "descripcion"=>"Debes unir las mujeres con su profesión o su logro",
-                "imagen"=>""
+                "descripcion"=>"En este juego deberas unir las mujeres con su profesión o su logro",
+                "imagen"=>"resources/assets/Fotos_Juegos/matching.png"
+            ),
+            array(
+                "nombre"=>"puzzle",
+                "descripcion"=>"En este juego deberas unir las distintas piezas que juntas mostraran a una mujer",
+                "imagen"=>"resources/assets/Fotos_Juegos/puzzle.png"
             ),
             array(
                 "nombre"=>"buscaminas",
-                "descripcion"=>"Debes unir las mujeres con su profesión o su logro",
-                "imagen"=>""
+                "descripcion"=>"En este juego deberas evitar las bombas para descubrir el rosotro de una mujer",
+                "imagen"=>"resources/assets/Fotos_Juegos/buscaminas.png"
+            ),
+            array(
+                "nombre"=>"Quien quiere ser millonaria?",
+                "descripcion"=>"En este juego deberas responder las preguntas de las distintas mujeres.",
+                "imagen"=>"resources/assets/Fotos_Juegos/quiz.png"
+            ),
+            array(
+                "nombre"=>"3 en raya",
+                "descripcion"=>"En este juego deberas ganar una partida de 3 en raya contra una inteligente maquina",
+                "imagen"=>"resources/assets/Fotos_Juegos/images.png"
             )
         );
     }
--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,6 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-<<<<<<< HEAD
-//Esta sera la ruta al controllador de la pestaña del modo historia
-Route::get('/modoHistoria', [modoHistoriaController::class, 'getmodoHistoria']);
-=======
 //Abrir nuevo enlace a menu juegos y juegos
 Route::get("/juegos/matching", function(){
     return view("home");
@@ -46,4 +42,3 @@ Route::get("/perfil",[GameController::class,"show"])->name("perfil");
 
 Route::get("/logout",[GameController::class,"show"])->name("logout");
 
->>>>>>> 84bfbec3bfe2636fc566db2311ede304ba5eb215
