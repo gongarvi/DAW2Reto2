3aebe9adc16d21f19da56d8cdc31f264157fde8e
Sayeeda12614 <sayeeda.txurdi@gmail.com>
2021-01-20 16:53:48 +0100

    20/01/2021

--- resources/views/perfil/edit.blade.php
+++ resources/views/perfil/edit.blade.php
@@ -13,45 +13,75 @@
         <div style="background-color:rgba(0,0,0,0.555);">
             <div class="m-5"> 
                 <form action="/actualizar/{{$usuario->id}}" method="POST">
+                    <fieldset class="form-group">
+                        <legend class="col-form-label col-sm-2 pt-0">Modificar datos</legend>
+                        <input type="hidden" value="datos" name="funcion">
+                        <img src="{{ (trim($usuario->foto)!=='')?asset('assets/Fotos_mujeres/'.$usuario->foto):asset('image/placeholder-usuario.png')}}" alt="" title="" class="rounded-circle mt-2" style="height: 200px;"/><br>
+                        <button class="btn btn-light m-2">Cambiar foto</button>
+                        <div class="d-flex flex-column">
+                            <div class="form-group"> 
+                                <div class="col-md-4 inputGroupContainer">
+                                <label for="name" class="text-light">Nombre: </label>
+                                    <div class="input-group">
+                                        <input  name="name" value="{{$usuario->name}}" class="form-control"  type="text">
+                                    </div>
+                                </div>
+                            </div>
 
-                    <input type="hidden" value="datos" name="funcion">
-                    <img src="{{ (trim($usuario->foto)!=='')?asset('assets/Fotos_mujeres/'.$usuario->foto):asset('image/placeholder-usuario.png')}}" alt="" title="" class="rounded-circle mt-2" style="height: 200px;"/><br>
-                    <button class="btn btn-secondary m-2">Cambiar foto</button>
-                    <div class="d-flex flex-column">
-                        <div class="form-group"> 
-                            <div class="col-md-4 inputGroupContainer">
-                                <div class="input-group">
-                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                    <input  name="name" value="{{$usuario->name}}" class="form-control"  type="text">
+                            <div class="form-group"> 
+                                <div class="col-md-4 inputGroupContainer">
+                                <label for="email" class="text-light">Correo: </label>
+                                    <div class="input-group">
+                                        <input readonly name="email" placeholder="{{$usuario->email}}" class="form-control"  type="email">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group"> 
+                                <div class="col-md-4 inputGroupContainer">
+                                <label for="password" class="text-warning">Escribe tu contraseña para confirmar</label>
+                                    <div class="input-group">
+                                        <input  name="password" placeholder="Contraseña" class="form-control"  type="password">
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        <input type="reset" class="btn btn-danger mb-2">
+                        <input type="submit" value="Actualizar Usuario" class="btn btn-primary mb-2">
+                    </fieldset>
+                </form>
 
-                        <div class="form-group"> 
-                            <div class="col-md-4 inputGroupContainer">
-                                <div class="input-group">
-                                    <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                                    <input readonly name="email" placeholder="{{$usuario->email}}" class="form-control"  type="email">
+                <form action="/actualizar/{{$usuario->id}}" method="POST">
+                    <fieldset class="form-group">
+                        <legend class="col-form-label col-sm-2 pt-0">Cambiar contraseña</legend>
+                        <input type="hidden" value="contrasena" name="funcion">
+                        <div class="d-flex flex-column">
+                            <div class="form-group"> 
+                                <div class="col-md-4 inputGroupContainer">
+                                    <div class="input-group">
+                                        <input  name="actual_password" placeholder="Contraseña actual" class="form-control"  type="password">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group"> 
+                                <div class="col-md-4 inputGroupContainer">
+                                    <div class="input-group">
+                                        <input  name="new_password" placeholder="Contraseña nueva" class="form-control"  type="password">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group"> 
+                                <div class="col-md-4 inputGroupContainer">
+                                    <div class="input-group">
+                                        <input  name="confirm_password" placeholder="Confirmar contraseña nueva" class="form-control"  type="password">
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="form-group"> 
-                            <div class="col-md-4 inputGroupContainer">
-                                <div class="input-group">
-                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                    <label for="password" class="text-warning">Escribe tu contraseña para confirmar</label>
-                                    <input  name="password" placeholder="Contraseña" class="form-control"  type="password">
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <input type="reset" class="btn btn-danger mb-2">
-                    <input type="submit" value="Actualizar Usuario" class="btn btn-primary mb-2">
+                        <input type="reset" class="btn btn-danger mb-2">
+                        <input type="submit" value="Actualizar Usuario" class="btn btn-primary mb-2">
+                    </fieldset>
                 </form>
             </div>
         </div>
