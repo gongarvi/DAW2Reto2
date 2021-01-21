a69a2e168554cb56dc242ee6b4bca5401f1ff8ee
EricEguski <ericeguskizaga@gmail.com>
2021-01-21 13:51:43 +0100

    Buscaminas0

--- resources/views/partials/include.blade.php
+++
@@ -1,9 +0,0 @@
-
-
-<!-- Styles -->
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-<!-- Scripts -->
-<script src="{{asset('js/app.js') }}" defer></script>
-
-
--- resources/views/ruleta.blade.php
+++
@@ -1,9 +0,0 @@
-@extends("layouts.page")
-
-@section("head-extras")
-
-@endsection
-
-@section("content")
-    <Ruleta/>
-@endsection
