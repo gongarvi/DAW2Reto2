428bd97fccc4efdbdff9ea7788d6a256799cb239
Sayeeda12614 <sayeeda.txurdi@gmail.com>
2021-01-22 16:52:57 +0100

    22/01/2021

--- resources/views/perfil/edit.blade.php
+++ resources/views/perfil/edit.blade.php
@@ -19,42 +19,40 @@
                 </div>
             @endif
                 <form action="/actualizarPerfil/{{$usuario->id}}" method="POST">
-                @csrf
-                    <fieldset class="form-group">
-                        <legend class="col-form-label col-sm-2 pt-0">Modificar datos</legend>
-                        <input type="hidden" value="datos" name="funcion">
-                        <img src="{{ (trim($usuario->foto)!=='')?asset('assets/Fotos_mujeres/'.$usuario->foto):asset('image/placeholder-usuario.png')}}" alt="" title="" class="rounded-circle mt-2" style="height: 200px;"/><br>
-                        <button class="btn btn-light m-2">Cambiar foto</button>
-                        <div class="d-flex flex-column">
-                            <div class="form-group"> 
-                                <div class="col-md-4 inputGroupContainer">
-                                <label for="name" class="text-light">Nombre: </label>
-                                    <div class="input-group">
-                                        <input  name="name" value="{{$usuario->name}}" class="form-control"  type="text">
-                                    </div>
-                                </div>
-                            </div>
+                    @csrf
+                    <h2>Cambiar nombre</h2>
+                        <div class="form-group">
+                            <input type="hidden" value="datos" name="funcion">
+                            <img src="{{ (trim($usuario->foto)!=='')?asset('assets/Fotos_mujeres/'.$usuario->foto):asset('image/placeholder-usuario.png')}}" alt="" title="" class="rounded-circle mt-2" style="height: 200px;"/><br>
+                            <button class="btn btn-light m-2">Cambiar foto</button>
+                        </div>
+                        
+                       
+                        <div class="form-group"> 
+                            <label for="name" class="text-light">Nombre: </label>
+                            <input  name="name" value="{{$usuario->name}}" class="form-control"  type="text">
+                        </div>
+                        
 
                             <div class="form-group"> 
-                                <div class="col-md-4 inputGroupContainer">
+                                
                                 <label for="email" class="text-light">Correo: </label>
-                                    <div class="input-group">
+                                   
                                         <input readonly name="email" placeholder="{{$usuario->email}}" class="form-control"  type="email">
-                                    </div>
-                                </div>
+                                    
+                               
                             </div>
                             <div class="form-group"> 
-                                <div class="col-md-4 inputGroupContainer">
+                                
                                 <label for="password" class="text-warning">Escribe tu contraseña para confirmar</label>
-                                    <div class="input-group">
+                                   
                                         <input  name="password" placeholder="Contraseña" class="form-control"  type="password">
-                                    </div>
-                                </div>
+                                   
+                                
                             </div>
                         </div>
                         <input type="reset" class="btn btn-danger mb-2">
                         <input type="submit" value="Actualizar Usuario" class="btn btn-primary mb-2">
-                    </fieldset>
                 </form>
 
                 <form action="/actualizarPerfil/{{$usuario->id}}" method="post">
